8b3ebe6b5efa644d3b5049261f36e033875be709
Mo_thunderz <76265418+mo-thunderz@users.noreply.github.com>
2021-09-11 11:35:28 +0200

    Add files via upload

--- Arduino/ESP32WebserverWebsocket/ESP32WebserverWebsocket.ino
+++ Arduino/ESP32WebserverWebsocket/ESP32WebserverWebsocket.ino
@@ -1,80 +1,88 @@
-// --------------------------------------------------
+// ---------------------------------------------------------------------------------------
 //
-// Code setting up a simple webserver on the ESP32. 
-// device used for tests: ESP32-WROOM-32D
+// Code for a simple webserver on the ESP32 (device used for tests: ESP32-WROOM-32D).
+// The code generates a random number on the ESP32 and uses Websockets to continuously
+// update the web-clients. Please check out also the link below where this code is upgraded
+// with JSON for more robust communication:
+// https://github.com/mo-thunderz/Esp32WifiPart2/tree/main/Arduino/ESP32WebserverWebsocketJson
 //
-// Install Websockets by Markus Sattler
+// For installation, the following libraries need to be installed:
+// * Websockets by Markus Sattler (can be tricky to find -> search for "Arduino Websockets"
 //
-// Written by mo thunderz (last update: 05.09.2021)
+// Refer to https://youtu.be/15X0WvGaVg8
 //
-// --------------------------------------------------
+// Written by mo thunderz (last update: 11.09.2021)
+//
+// ---------------------------------------------------------------------------------------
 
-#include <WiFi.h>
-#include <WebServer.h> 
-#include <WebSocketsServer.h>  // Include Websocket Library
+#include <WiFi.h>                                     // needed to connect to WiFi
+#include <WebServer.h>                                // needed to create a simple webserver
+#include <WebSocketsServer.h>                         // needed for instant communication between client and server through Websockets
 
 // SSID and password of Wifi connection:
 const char* ssid = "TYPE_YOUR_SSID_HERE";
 const char* password = "TYPE_YOUR_PASSWORD_HERE";
 
+// The String below "webpage" contains the complete HTML code that is sent to the client whenever someone connects to the webserver
 String website = "<!DOCTYPE html><html><head><title>Page Title</title></head><body style='background-color: #EEEEEE;'><span style='color: #003366;'><h1>Lets generate a random number</h1><p>The random number is: <span id='rand'>-</span></p><p><button type='button' id='BTN_SEND_BACK'>Send info to ESP32</button></p></span></body><script> var Socket; document.getElementById('BTN_SEND_BACK').addEventListener('click', button_send_back); function init() { Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event) { processCommand(event); }; } function button_send_back() { Socket.send('Sending back some random stuff'); } function processCommand(event) { document.getElementById('rand').innerHTML = event.data; console.log(event.data); } window.onload = function(event) { init(); }</script></html>";
 
-int randomval = random(100);
-int interval = 1000; 
-unsigned long previousMillis = 0; 
+// We want to periodically send values to the clients, so we need to define an "interval" and remember the last time we sent data to the client (with "previousMillis")
+int interval = 1000;                                  // send data to the client every 1000ms -> 1s
+unsigned long previousMillis = 0;                     // we use the "millis()" command for time reference and this will output an unsigned long
 
-WebServer server(80);
-WebSocketsServer webSocket = WebSocketsServer(81); 
+// Initialization of webserver and websocket
+WebServer server(80);                                 // the server uses port 80 (standard port for websites
+WebSocketsServer webSocket = WebSocketsServer(81);    // the websocket uses port 81 (standard port for websockets
 
 void setup() {
-  Serial.begin(115200);                 
+  Serial.begin(115200);                               // init serial port for debugging           
  
-  WiFi.begin(ssid, password);
-  Serial.println("Establishing connection to WiFi with SSID: " + String(ssid));
+  WiFi.begin(ssid, password);                         // start WiFi interface
+  Serial.println("Establishing connection to WiFi with SSID: " + String(ssid));     // print SSID to the serial interface for debugging
  
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {             // wait until WiFi is connected
     delay(1000);
     Serial.print(".");
   }
   Serial.print("Connected to network with IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());                     // show IP address that the ESP32 has received from router
   
-  server.on("/", []() {
-  server.send(200, "text\html", website);
+  server.on("/", []() {                               // define here wat the webserver needs to do
+  server.send(200, "text\html", website);             //    -> it needs to send out the HTML string "webpage" to the client
   });
-  server.begin(); // init the server
+  server.begin();                                     // start server
   
-  webSocket.begin(); 
-  webSocket.onEvent(webSocketEvent);
+  webSocket.begin();                                  // start websocket
+  webSocket.onEvent(webSocketEvent);                  // define a callback function -> what does the ESP32 need to do when an event from the websocket is received? -> run function "webSocketEvent()"
 }
 
 void loop() {
-  server.handleClient();  // webserver methode that handles all Client
-  webSocket.loop(); 
+  server.handleClient();                              // Needed for teh webserver to handle all clients
+  webSocket.loop();                                   // Update function for the webSockets 
   
-  unsigned long currentMillis = millis(); // call millis  and Get snapshot of time
-  if ((unsigned long)(currentMillis - previousMillis) >= interval) { // How much time has passed, accounting for rollover with subtraction!
-    String str = String(random(100)); 
-    int str_len = str.length() + 1; 
+  unsigned long now = millis();                       // read out the current "time" ("millis()" gives the time in ms since the Arduino started)
+  if ((unsigned long)(now - previousMillis) >= interval) { // check if "interval" ms has passed since last time the clients were updated
+    
+    String str = String(random(100));                 // create a random value
+    int str_len = str.length() + 1;                   
     char char_array[str_len];
-    str.toCharArray(char_array, str_len);
-    webSocket.broadcastTXT(char_array);
-    previousMillis = currentMillis;
+    str.toCharArray(char_array, str_len);             // convert to char array
+    webSocket.broadcastTXT(char_array);               // send char_array to clients
+    previousMillis = now;                             // reset previousMillis
   }
 }
 
-void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {
-  switch (type) {
-    case WStype_DISCONNECTED: // enum that read status this is used for debugging.
-      Serial.println("Client Disconnected");
+void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {      // the parameters of this callback function are always the same -> num: id of the client who send the event, type: type of message, payload: actual data sent and length: length of payload
+  switch (type) {                                     // switch on the type of information sent
+    case WStype_DISCONNECTED:                         // if a client is disconnected, then type == WStype_DISCONNECTED
+      Serial.println("Client " + String(num) + " disconnected");
       break;
-    case WStype_CONNECTED:  // Check if a WebSocket client is connected or not
-      Serial.println("Client Connected");
-      // add code here what to do when connected
-      
+    case WStype_CONNECTED:                            // if a client is connected, then type == WStype_CONNECTED
+      Serial.println("Client " + String(num) + " connected");
+      // optionally you can add code here what to do when connected
       break;
-    case WStype_TEXT: // check response from client
-      for (int i=0; i<length; i++) {    
+    case WStype_TEXT:                                 // if a client has sent data, then type == WStype_TEXT
+      for (int i=0; i<length; i++) {                  // print received data from client
         Serial.print((char)payload[i]);
       }
       Serial.println("");
